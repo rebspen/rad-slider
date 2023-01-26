4c8012a27944c9f4f7b3538209d4b1a92f8171e7
Rebecca Spence <rebecca.spence@radically.digital>
2023-01-26 16:15:29 +0000

    reformatted slider

---
+++ sources/App/Utils/Loader.js
@@ -0,0 +1,64 @@
+import { Texture } from "ogl";
+import EventEmitter from "./EventEmitter";
+import App from "../App";
+
+export default class Loader extends EventEmitter {
+  constructor() {
+    super();
+
+    this.app = new App();
+    this.gl = this.app.renderer.gl;
+    this.articles = this.app.articles;
+
+    this.loaded = 0;
+    this.toLoad = this.articles.length;
+
+    this.cache = {};
+
+    this.loadAll();
+  }
+
+  loadAll() {
+    this.articles.forEach((article) => {
+      this.load(article.image, article);
+      this.loadHover(article.hover, article);
+    });
+  }
+
+  loadHover(src, article) {
+    const textureMask = new Texture(this.gl, { generateMipmaps: false });
+    const image = new Image();
+    image.src = src;
+
+    image.onload = () => {
+      textureMask.image = image;
+
+      article.texture = textureMask;
+      article.hover = image;
+
+      this.checkFinish();
+    };
+  }
+
+  load(src, article) {
+    const textureMask = new Texture(this.gl, { generateMipmaps: false });
+    const image = new Image();
+    image.src = src;
+
+    image.onload = () => {
+      textureMask.image = image;
+      this.loaded++;
+
+      article.texture = textureMask;
+      article.image = image;
+
+      this.checkFinish();
+    };
+  }
+
+  checkFinish() {
+    if (this.loaded === this.toLoad) {
+      this.trigger("loaded");
+    }
+  }
+}
---
+++ sources/App/articles.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: "Article 0",
+    image: "/assets/example-0.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 1",
+    image: "/assets/example-1.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 2",
+    image: "/assets/example-2.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 3",
+    image: "/assets/example-3.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 4",
+    image: "/assets/example-4.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 5",
+    image: "/assets/example-5.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 6",
+    image: "/assets/example-6.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 7",
+    image: "/assets/example-7.webp",
+    hover: "/assets/example-0.webp"
+  },
+  {
+    title: "Article 8",
+    image: "/assets/example-8.webp",
+    hover: "/assets/example-0.webp"
+  }
+];
